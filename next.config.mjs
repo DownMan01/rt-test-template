0a26ffdf71b30cd473774594153e5f52b2daec4c
J <98541904+DownMan01@users.noreply.github.com>
2025-11-14 01:00:33 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -20,8 +20,8 @@ const nextConfig = {
     webpackBuildWorker: true,
     parallelServerBuildTraces: true,
     parallelServerCompiles: true,
-    serverComponentsExternalPackages: ['puppeteer'],
   },
+  serverExternalPackages: ['puppeteer']
 }
 
 mergeConfig(nextConfig, userConfig)
